469331e8245ff38e2a74c7671549c303157f14cf
Yacin Belmihoub-Martel <yacin.belmihoubmartel@gmail.com>
2022-05-09 17:29:37 -0400

    Adjusted setup

--- setup_init.sh
+++ setup_init.sh
@@ -13,10 +13,3 @@ sudo chmod +x run.sh
 sudo chmod +x kill.sh
 
 sudo mkdir -p /ram
-
-sudo ln -sf /ram/pr.jpg _webDisplay/pr.jpg
-sudo ln -sf /ram/DOA_value.html _webDisplay/DOA_value.html
-sudo ln -sf /ram/spectrum.jpg _webDisplay/spectrum.jpg
-sudo ln -sf /ram/sync.jpg _webDisplay/sync.jpg
-sudo ln -sf /ram/doa.jpg _webDisplay/doa.jpg
-sudo ln -sf /ram/pr.jpg _webDisplay/pr.jpg
